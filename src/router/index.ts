b0e57793f899e9b71aa20bd84653b4920e13a7f2
NIKITA <sinrulyu@gmail.com>
2025-05-19 22:50:38 +0800

    publish

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import {createRouter, createWebHashHistory, RouteRecordRaw} from 'vue-router';
+import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router';
 import {globalWording} from '@assets/wording/global/menu.ts';
 
 const routes: Array<RouteRecordRaw> = [
@@ -53,7 +53,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-	history: createWebHashHistory(),
+	history: createWebHistory(),
 	routes: routes,
 	scrollBehavior() {
 		return {top: 0};
