38095cf26315bb8bd317e73b8beba9fc02ee1516
nikitamacbook <sinrulyu@gmail.com>
2025-06-08 15:14:38 +0800

    首頁 vimeo

--- src/views/home/components/Section1.vue
+++ src/views/home/components/Section1.vue
@@ -10,17 +10,35 @@
 				</SectionDescription>
 			</div>
 			<div class="col-span-6">
-				<img class="w-full h-full object-cover" alt="section1" src="@/assets/img/home/section1.png" />
+				<div style="padding: 56.25% 0 0 0; position: relative">
+					<iframe
+						@load="onIframeLoad"
+						:src="iframeSrc"
+						class="absolute top-0 left-0 w-full h-full"
+						allow="autoplay; fullscreen; picture-in-picture"
+						frameborder="0"
+						title="Singing Cicadas Showreel">
+					</iframe>
+				</div>
+				<div v-if="!isLoaded">Loding...</div>
 			</div>
 		</div>
 	</SectionContainer>
 </template>
-
 <script lang="ts" setup>
+import {ref} from 'vue';
 import SectionContainer from '@/components/layout/SectionContainer.vue';
 import SectionTitle from '@components/atoms/text/SectionTitle.vue';
 import SectionDescription from '@components/atoms/text/SectionDescription.vue';
 import {contentText} from '@assets/wording/home/text.ts';
+
+const isLoaded = ref(false);
+const iframeSrc = 'https://player.vimeo.com/video/1059379503?autoplay=1&muted=1&title=0&byline=0&portrait=0&controls=0&background=1';
+
+function onIframeLoad() {
+	isLoaded.value = true;
+	console.log('🎞️ iframe loaded!');
+}
 </script>
 
 <style lang="scss" scoped></style>
