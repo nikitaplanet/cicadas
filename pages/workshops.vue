ce36ad96318c8830ef0056919a3f616f41b71280
nikitamacbook <sinrulyu@gmail.com>
2025-07-09 01:32:02 +0800

    workshops hide

--- pages/workshops.vue
+++ pages/workshops.vue
@@ -1,23 +1,24 @@
 <template>
-	<div class="w-full min-h-screen bg-surface-def">
-		<LandingSection />
+	workshops
+<!--	<div class="w-full min-h-screen bg-surface-def">-->
+<!--		<LandingSection />-->
 
-		<LogoSection />
+<!--		<LogoSection />-->
 
-		<QuoteSection />
-		<!--		<QuoteSwiper :contentList="workshopsWording.quoteList" />-->
+<!--		<QuoteSection />-->
+<!--		&lt;!&ndash;		<QuoteSwiper :contentList="workshopsWording.quoteList" />&ndash;&gt;-->
 
-		<WorkshopsSwiper />
-	</div>
+<!--		<WorkshopsSwiper />-->
+<!--	</div>-->
 </template>
 
 <script lang="ts" setup>
-import LandingSection from '@/components/pages/workshops/LandingSection.vue';
-import LogoSection from '@/components/pages/workshops/LogoSection.vue';
-import WorkshopsSwiper from '@/components/pages/workshops/WorkshopsSwiper.vue';
-// import QuoteSwiper from '@/components/pages/workshops/QuoteSwiper.vue';
-// import {workshopsWording} from '@/assets/wording/workshops/text';
-import QuoteSection from '@/components/pages/workshops/QuoteSection.vue';
+// import LandingSection from '@/components/pages/workshops/LandingSection.vue';
+// import LogoSection from '@/components/pages/workshops/LogoSection.vue';
+// import WorkshopsSwiper from '@/components/pages/workshops/WorkshopsSwiper.vue';
+// // import QuoteSwiper from '@/components/pages/workshops/QuoteSwiper.vue';
+// // import {workshopsWording} from '@/assets/wording/workshops/text';
+// import QuoteSection from '@/components/pages/workshops/QuoteSection.vue';
 </script>
 
 <style lang="scss" scoped></style>
