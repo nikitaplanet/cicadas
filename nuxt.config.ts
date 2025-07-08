2e6644866c768c4f0e8634483276f98d22480d98
nikitamacbook <sinrulyu@gmail.com>
2025-07-09 01:36:05 +0800

    移除head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,24 +8,6 @@ export default defineNuxtConfig({
 	},
 	app: {
 		baseURL: '/',
-		head: {
-			link: [
-				{
-					rel: 'preconnect',
-					href: 'https://fonts.googleapis.com'
-				},
-				{
-					rel: 'preconnect',
-					href: 'https://fonts.gstatic.com',
-					crossorigin: 'anonymous'
-				},
-				{
-					rel: 'preload',
-					href: 'https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&family=Besley:ital,wght@0,400..900;1,400..900&family=Homemade+Apple&family=Hanken+Grotesk&display=swap',
-					as: 'style'
-				}
-			]
-		}
 	},
 	compatibilityDate: '2025-05-15',
 	devtools: {enabled: false},
