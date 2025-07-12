4df4e2880322e461d786c5603996d898c3109274
nikitamacbook <sinrulyu@gmail.com>
2025-07-12 23:20:29 +0800

    remove cookie bot

--- app.vue
+++ app.vue
@@ -48,13 +48,14 @@ useHead({
 	],
 	script: [
 		{src: 'https://tally.so/widgets/embed.js', async: true},
-		{
-			id: 'Cookiebot',
-			src: 'https://consent.cookiebot.com/uc.js',
-			type: 'text/javascript',
-			'data-cbid': '5efa9b8a-2cc4-485e-9d86-54b078df59e8',
-			'data-blockingmode': 'auto',
-		},
+		// {
+		// 	id: 'Cookiebot',
+		// 	src: 'https://consent.cookiebot.com/uc.js',
+		// 	type: 'text/javascript',
+		// 	'data-cbid': '5efa9b8a-2cc4-485e-9d86-54b078df59e8',
+		// 	'data-blockingmode': 'auto',
+		// 	async: true
+		// },
 	],
 });
 
