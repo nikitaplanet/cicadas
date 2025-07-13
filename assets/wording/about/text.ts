269c27bb8e024cecb7ca7c51c7e15073af245393
nikitamacbook <sinrulyu@gmail.com>
2025-07-13 17:40:37 +0800

    about our team

--- assets/wording/about/text.ts
+++ assets/wording/about/text.ts
@@ -5,6 +5,7 @@ import Marias from '@/assets/img/about/workWith/Marias.svg';
 import Tylor from '@/assets/img/about/workWith/Tylor.svg';
 
 import Sharon from '@/assets/img/about/profile/sharon.svg';
+import SharonMobile from '@/assets/img/about/profile/sharon_m.svg';
 import Hanyan from '@/assets/img/about/profile/hanyan.svg';
 import HannaShin from '@/assets/img/about/profile/hanna.svg';
 import {OUR_TEAM_INFO} from '@/assets/js/enum/about';
@@ -26,6 +27,7 @@ export const aboutWording = {
 			{
 				id: OUR_TEAM_INFO.SHARON,
 				img: Sharon,
+				imgMobile: SharonMobile,
 				name: 'Sharon Yeung',
 				description:
 					'I’m a multimedia storyteller and community builder based in Hong Kong, who loves creating engaging narratives across diverse platforms. From films and web games to VR and interactive experiences, I’m passionate about inspiring and connecting communities. I’ve had the opportunity for training with organizations like Sundance, and Berlinale, which really honed my skills as a storyteller and producer. I’m committed to creating independent documentaries that explore complex subjects and diverse perspectives.',
@@ -33,6 +35,7 @@ export const aboutWording = {
 			{
 				id: OUR_TEAM_INFO.HANYAN,
 				img: Hanyan,
+				imgMobile: Hanyan,
 				name: 'Han Yan Yuen',
 				description:
 					'I am an impact strategist based in Taipei/ Hong Kong. I founded an NGO in Asia and was the Director of Communications at a global NGO. I’ve worked with complex audiences in Asia across a mosaic of cultures, languages and digital access levels. By tailoring strategies to each unique context, I deliver impactful campaigns to address a broad range of social issues. I was recognized by UN Women China as the Women of the Year in Media in 2011.',
@@ -40,6 +43,7 @@ export const aboutWording = {
 			{
 				id: OUR_TEAM_INFO.HANNA,
 				img: HannaShin,
+				imgMobile: HannaShin,
 				name: 'Hanna Shin',
 				description:
 					'I am an experienced campaign strategist with a strong background in corporate social responsibility and human rights in Korea. I specialize in strategic planning and stakeholder engagement, and have worked with international organizations including the British Council, Médecins Sans Frontières and Amnesty International.',
--- components/pages/about/OurTeamInfoOverlay.vue
+++ components/pages/about/OurTeamInfoOverlay.vue
@@ -1,25 +1,54 @@
 <template>
-	<div class="ourTeamOverlay w-full h-screen flex flex-col justify-center items-center fixed top-0 left-0 z-30 pt-14 pb-24 px-10">
-		<div class="w-full max-w-[1300px] flex justify-end items-center">
-			<button @click="handleClose" class="w-11 h-11 flex justify-end items-start" type="button">
-				<img class="w-6 h-6" alt="Close" src="@/assets/img/components/overlay/close.svg" />
-			</button>
+	<!--Desktop-->
+	<div>
+		<div class="ourTeamOverlay w-full h-screen hidden lg:flex flex-col justify-center items-center fixed top-0 left-0 z-30 pt-14 pb-24 px-10">
+			<div class="w-full max-w-[1300px] flex justify-end items-center">
+				<button @click="handleClose" class="w-11 h-11 flex justify-end items-start" type="button">
+					<img class="w-6 h-6" alt="Close" src="@/assets/img/components/overlay/close.svg" />
+				</button>
+			</div>
+			<div class="w-full max-w-[1300px] gap-8 flex flex-col md:flex-row justify-center items-center">
+				<div class="flex-shrink-0 w-[35%]">
+					<img :alt="profileInfo.name" :src="profileInfo.img" class="w-full h-auto" />
+				</div>
+
+				<div class="relative w-full h-[580px] lg:h-[430px]">
+					<img class="object-fit w-full h-full block lg:hidden" alt="bg" src="@/assets/img/about/ourTeamInfoBg.svg" />
+					<img class="object-fit w-full h-full hidden lg:block xl:hidden" alt="bg" src="@/assets/img/about/ourTeamInfoBg_wMedium.svg" />
+					<img class="object-fit w-full h-full hidden lg:hidden xl:block" alt="bg" src="@/assets/img/about/ourTeamInfoBg_wLong.svg" />
+
+					<div class="absolute w-full h-full top-0 left-0 px-20 pt-48 lg:px-20 lg:pt-32 xl:pt-36">
+						<p class="font-semibold text-h4 font-h4 mb-4">{{ profileInfo.name }}</p>
+						<p class="font-body text-body font-medium">
+							{{ profileInfo.description }}
+						</p>
+					</div>
+				</div>
+			</div>
 		</div>
-		<div class="w-full max-w-[1300px] gap-8 flex flex-row justify-center items-center">
-			<div class="flex-shrink-0 w-[35%]">
-				<img :src="profileInfo.img" class="w-full h-auto" alt="Sharon 插圖" />
+
+		<!--Mobile-->
+		<div class="ourTeamOverlay w-full h-screen flex flex-col lg:hidden fixed top-0 left-0 z-30">
+			<div class="w-full h-[84px] flex justify-end items-center px-10 pt-10">
+				<button @click="handleClose" class="w-11 h-11 flex justify-end items-start" type="button">
+					<img class="w-[30px] h-[30px]" alt="Close" src="@/assets/img/components/overlay/close.svg" />
+				</button>
+			</div>
+			<!--Img-->
+			<div class="flex justify-center items-center px-6 h-[268px]">
+				<img :alt="profileInfo.name" :src="profileInfo.imgMobile" class="w-full max-h-[268px] h-auto" />
 			</div>
 
-			<div class="relative w-full h-[580px] lg:h-[430px]">
-				<img class="object-fit w-full h-full block lg:hidden" alt="bg" src="@/assets/img/about/ourTeamInfoBg.svg" />
-				<img class="object-fit w-full h-full hidden lg:block xl:hidden" alt="bg" src="@/assets/img/about/ourTeamInfoBg_wMedium.svg" />
-				<img class="object-fit w-full h-full hidden lg:hidden xl:block" alt="bg" src="@/assets/img/about/ourTeamInfoBg_wLong.svg" />
-
-				<div class="absolute w-full h-full top-0 left-0 px-20 pt-48 lg:px-20 lg:pt-32 xl:pt-36">
-					<p class="font-semibold text-h4 font-h4 mb-4">{{ profileInfo.name }}</p>
-					<p class="font-body text-body font-medium">
-						{{ profileInfo.description }}
-					</p>
+			<!--Content-->
+			<div class="ourTeamOverlay__content__mobile bg-cover flex-grow px-6 pb-10 pt-5">
+				<div class="relative w-full h-full bg-surface-tertiary px-4 py-10">
+					<img class="absolute top-[-10px] left-0" alt="top" src="@/assets/img/about/bgimage/bottom_m.svg" />
+					<div class="w-full h-full overflow-y-auto overscroll-contain">
+						<div class="text-scaleLG font-h4 font-semibold">{{ profileInfo.name }}</div>
+						<div class="mt-5 text-body font-body font-medium">
+							{{ profileInfo.description }}{{ profileInfo.description }}{{ profileInfo.description }}{{ profileInfo.description }}
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -65,6 +94,17 @@ const profileInfo = reactive(aboutWording.ourTeam.ourTeamList.find((item) => ite
 
 	&__bgImage {
 		background: url('@/assets/img/about/ourTeamInfoBg.svg') no-repeat;
+	}
+
+	&__content {
+		&__mobile {
+			height: calc(100vh - 84px - 268px);
+			-webkit-overflow-scrolling: touch;
+
+			&__info {
+				height: calc(100vh - 84px - 268px - 140px);
+			}
+		}
 	}
 }
 </style>
